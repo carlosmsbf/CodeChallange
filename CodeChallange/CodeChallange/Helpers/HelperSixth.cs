a88b490339b64af56ad2edc198f3999a67215f9b
carlosmsbf <carlosmsbf@gmail.com>
2022-06-04 12:46:17 -0300

    More soluctions done

---
+++ CodeChallange/CodeChallange/Helpers/HelperSixth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallange.Helpers
+{
+    public class HelperSixth
+    {
+
+        private int Number { get; set; }
+        private int Quantity { get; set; }
+
+
+        public HelperSixth(int number, int quantity)
+        {
+            Quantity = quantity;
+            Number = number;
+        }
+
+
+    }
+}
--- CodeChallange/CodeChallange/Program.cs
+++ CodeChallange/CodeChallange/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CodeChallange.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -32,15 +33,17 @@ namespace CodeChallange
                 Console.WriteLine("2.Return the number(count) of vowels in the given string.");
                 Console.WriteLine("3.Returning a string without vowel.");
                 Console.WriteLine("4.Create Phone Number");
-
-
+                Console.WriteLine("5.Square Every Digit.");
+                Console.WriteLine("6.Find the odd int");
+                Console.WriteLine("7.Stop gninnipS My sdroW!");
 
                 try
                 {
                     menuOption = int.Parse(Console.ReadLine());
 
                 }
-                catch {
+                catch
+                {
                     menuOption = 0;
                 }
 
@@ -50,21 +53,21 @@ namespace CodeChallange
                     // 1. Multiples of 3 or 5.
                     case 1:
                         FirstCase();
-               
+
                         break;
 
 
-                    //Return the number(count) of vowels in the given string.
+                    //2.Return the number(count) of vowels in the given string.
 
                     case 2:
                         SecondCase();
-   
+
                         break;
 
-                    //Returning a string without vowel.
+                    //3.Returning a string without vowel.
                     case 3:
                         ThirdCase();
-         
+
                         break;
 
                     case 4:
@@ -72,6 +75,22 @@ namespace CodeChallange
 
                         break;
 
+                    //4.Square Every Digit
+                    case 5:
+                        Fifith();
+                        break;
+                    //5.Find the odd int
+                    case 6:
+                        Sixth();
+                        break;
+
+                    //6.Stop gninnipS My sdroW!
+                    case 7:
+                        Seventh();
+                        break;
+
+
+                    //7.End of Program
                     case 99:
 
                         isEnd = true;
@@ -80,14 +99,10 @@ namespace CodeChallange
 
                         Console.WriteLine("\n Please, select again.");
                         break;
-
                 }
 
                 // };
             };
-
-
-
         }
 
         //Multiples of 3 or 5."
@@ -124,16 +139,10 @@ namespace CodeChallange
                 }
                 catch
                 {
-
                     isNumber = false;
                     Console.WriteLine("It has to be a numbers");
                 }
-
             }
-
-            
-
-
         }
 
         private static void SecondCase()
@@ -171,6 +180,7 @@ namespace CodeChallange
         private static void ThirdCase()
         {
             char[] charArray = new char[0];
+
             int quantity = 0;
             string word = "";
             string pattern = @"[AEIOU-aeiou]";
@@ -204,14 +214,14 @@ namespace CodeChallange
             bool Has10digitis = false;
             int quantity = 0;
             string pattern = @"[a-zA-Z]";
-            string phoneNumber ="";
+            string phoneNumber = "";
 
             while (Has10digitis == false)
             {
                 try
                 {
                     Console.WriteLine("Please, write a phone number of 10 digits");
-                    userInput =Console.ReadLine();
+                    userInput = Console.ReadLine();
                     quantity = (userInput.Length);
                     if (quantity < 10)
                     {
@@ -220,7 +230,7 @@ namespace CodeChallange
                     }
                     else
                     {
-                        if(Regex.IsMatch(userInput,pattern))
+                        if (Regex.IsMatch(userInput, pattern))
                         {
                             Has10digitis = false;
                             Console.WriteLine("It has to be numbers instead of letters");
@@ -231,13 +241,13 @@ namespace CodeChallange
                             Has10digitis = true;
                             Array.Resize<char>(ref charArray, quantity);
                             charArray = userInput.ToCharArray();
-                            foreach(char number in charArray)
+                            foreach (char number in charArray)
                             {
                                 turns++;
 
                                 if (turns == 1)
                                     phoneNumber += "(";
-                                else if(turns == 4)
+                                else if (turns == 4)
                                     phoneNumber += ")";
                                 else if (turns == 7)
                                     phoneNumber += "-";
@@ -259,9 +269,161 @@ namespace CodeChallange
             Console.WriteLine($"{phoneNumber}");
         }
 
+        //Square Every Digit
+        private static void Fifith()
+        {
 
+            string userInput;
+            int quantity;
+            char[] charArray = new char[0];
+            string result = "";
+
+
+            Console.WriteLine("Please, enter a integer number");
+
+            userInput = Console.ReadLine();
+
+            quantity = userInput.Length;
+            Array.Resize<char>(ref charArray, quantity);
+            charArray = userInput.ToCharArray();
+            foreach (char number in charArray)
+            {
+                result += int.Parse(number.ToString()) * int.Parse(number.ToString());
+            }
+
+
+            Console.WriteLine($"{result}");
+        }
+
+        //Find the odd int
+        private static void Sixth()
+        {
+            string userInput;
+            int quantity;
+            char[] charArray = new char[0];
+            List<int> quantityResult = new List<int>();
+            List<int> result = new List<int>();
+
+            Console.WriteLine("Please, enter a integer number");
+
+            userInput = Console.ReadLine();
+
+            quantity = userInput.Length;
+            Array.Resize<char>(ref charArray, quantity);
+            charArray = userInput.ToCharArray();
+
+            foreach (char number in charArray)
+            {
+                if (!result.Contains(int.Parse(number.ToString())))
+                {
+                    result.Add(int.Parse(number.ToString()));
+                    quantityResult.Add(1);
+                }
+                else
+                {
+                    for (int x = 0; x < result.Count; x++)
+                    {
+
+
+                        if (result[x].Equals(int.Parse(number.ToString())))
+                        {
+                            quantityResult[x] = quantityResult[x] + 1;
+                            break;
+                        }
+                    }
+                }
+
+            }
+
+            for (int i = 0; i < result.Count; i++)
+            {
+
+                if (!(quantityResult[i] % 2 == 0))
+                    if(quantityResult[i] == 1)
+                    Console.WriteLine($"number: {result[i]} has {quantityResult[i]} odd number");
+                    else
+                    {
+                        Console.WriteLine($"number: {result[i]} has {quantityResult[i]} odds numbers");
+                    }
+            }
+        }
+
+        //Stop gninnipS My sdroW!
+        private static void Seventh()
+        {
+
+            string userInput;
+            int quantity;
+            string str = "";
+            char[] charArray = new char[0];
+            char[] wordArray = new char[0];
+            //List<string> reverseWord = new List<string>();
+            string reverseWord = "";
+            List<string> words = new List<string>();
+            int size = 0;
+            string pattern = @"[a-zA-Z]";
+
+            Random random = new Random();
+            int num = 0;
+
+
+            Console.WriteLine("Please, enter a phrase.");
+
+            userInput = Console.ReadLine();
+
+            quantity = userInput.Length;
+
+            Array.Resize<char>(ref charArray, quantity);
+            charArray = userInput.ToCharArray();
+
+            size = charArray.Length;
+            int a = 0;
+            foreach(char letter in charArray)
+            {
+                a++;
+                if (a == size)
+                 { str += letter;
+                     words.Add(str);
+                     str = "";
+                 }
+                else if (Regex.IsMatch(letter.ToString(), pattern))
+                {
+                    str += letter;
+                }
+                
+                else
+                {
+                    words.Add(str);
+                    str = "";
+                }
+            }
+
+            num = random.Next(0, words.Count);
+
+            quantity = words[num].Length;
+            Array.Resize<char>(ref wordArray, quantity);
+
+            wordArray = words[num].ToCharArray();
+
+                for (int i = wordArray.Length - 1; i >= 0; --i)
+            {
+
+                reverseWord += wordArray[i].ToString();
+            }
+            words[num] = reverseWord;
+
+
+            str = "";
+
+            foreach(string word in words)
+            {
+                str += word + " ";
+            }
+
+            Console.WriteLine($"{str.Trim()}");
+
+        }
     }
-
 
 }
 
